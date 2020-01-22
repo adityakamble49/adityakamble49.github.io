f3103af6624dda635437d1b121b018848daf8a44
Aditya Kamble <adityakamble49@gmail.com>
2020-01-22 18:24:01 -0500

    Contributions added | Updated theme

---
+++ js/skillbar.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.html').animate({width:'90%'},2000);
+    $('.css').animate({width:'75%'},2000);
+    $('.js').animate({width:'50%'},2000);
+    $('.php').animate({width:'70%'},2000);
+    $('.ajax').animate({width:'45%'},2000);
+});
